9acc6ab061457218027692408089ecaf2e7d2031
Emmanuel Sierra <esierradev@gmail.com>
2026-02-28 22:21:52 -0400

    Cambios en modelo de IA para desplegar en NetLify

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -35,7 +35,7 @@ export const handler = async (event) => {
   try {
     // Usamos el fetch global y nativo de Netlify
     const res = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
